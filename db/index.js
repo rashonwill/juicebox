f0c2619d7a2ce2495d27216376ee806acb867b11
rashonwill <williams_rashon92@yahoo.com>
2021-04-15 20:16:07 -0500

    Module 1 Changes

--- db/index.js
+++ db/index.js
@@ -2,8 +2,40 @@ const { Client } = require('pg');
 
 const client = new Client('postgres://localhost:5432/juicebox-db');
 
-module.exports = {
-  client,
+// module.exports = {
+//   client,
+// }
+
+// async function getAllUsers() {
+//   const { rows } = await client.query(
+//     `SELECT id, username 
+//     FROM users;
+//   `);
+
+//   return rows;
+// }
+
+// // and export them
+// module.exports = {
+//   client,
+//   getAllUsers,
+// }
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+    INSERT INTO users(username, password) 
+    VALUES($1, $2) 
+    ON CONFLICT (username) DO NOTHING 
+    RETURNING *;
+    `);
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
 }
 
-//cHANGES
+// later
+module.exports = {
+  // add createUser here!
+}
